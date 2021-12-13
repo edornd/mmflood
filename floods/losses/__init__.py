Hardcoded clipping (looks like it works better _after_ normalization), fixed ClipNormalize, small refactoring

floods/losses/__init__.py
@@ -11,10 +11,10 @@ from floods.losses.functional import lovasz_softmax, one_hot_batch, smooth_weigh
 class CombinedLoss(nn.Module):
     """Simply combines two losses into a single one, with weights.
     """
-    def __init__(self, criterion_a: Callable, criterion_b: Callable, alpha: float = 0.5):
+    def __init__(self, criterion_a: Callable, criterion_b: Callable, alpha: float = 0.5, **kwargs: dict):
         super().__init__()
-        self.criterion_a = criterion_a()
-        self.criterion_b = criterion_b()
+        self.criterion_a = criterion_a(**kwargs)
+        self.criterion_b = criterion_b(**kwargs)
         self.alpha = alpha
 
     def forward(self, preds: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:

floods/transforms.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Sequence
+from typing import Sequence, Union
 
 import numpy as np
 import torch
@@ -37,15 +37,19 @@ class ClipNormalize(Normalize):
     def __init__(self,
                  mean: tuple,
                  std: tuple,
-                 clip_min: tuple,
-                 clip_max: tuple,
+                 clip_min: Union[float, tuple],
+                 clip_max: Union[float, tuple],
                  max_pixel_value: float = 1.0,
                  always_apply: bool = False,
                  p: float = 1.0):
         super().__init__(mean=mean, std=std, max_pixel_value=max_pixel_value, always_apply=always_apply, p=p)
-        self.min = clip_min
-        self.max = clip_max
+        self.clip_min = clip_min
+        self.clip_max = clip_max
 
-    def __call__(self, image, **params) -> Dict[str, Any]:
-        image = np.clip(image, self.min, self.max)
-        return super().__call__(image=image, **params)
+    def apply(self, image: np.ndarray, **params) -> np.ndarray:
+        result = super().apply(image=image, **params)
+        return np.clip(result, self.clip_min, self.clip_max)
+
+    def get_transform_init_args_names(self):
+        parent = list(super().get_transform_init_args_names())
+        return tuple(parent + ["clip_min", "clip_max"])

floods/utils/gis.py
@@ -52,7 +52,7 @@ def write_window(window: Window, source: DatasetReader, path: Path, transform: A
 
 def rgb_ratio(sar_image: np.ndarray,
               channels_first: bool = False,
-              weights: tuple = (1.0, 2.0, 0.1),
+              weights: tuple = (1.0, 1.0, 0.2),
               scale: float = 255,
               dtype: np.dtype = np.uint8) -> np.ndarray:
     assert sar_image.ndim == 3, "batch not supported"

floods/utils/tiling/functional.py
@@ -1,5 +1,5 @@
-from glob import glob
-from typing import Generator, Union
+from pathlib import Path
+from typing import Generator, List, Union
 
 import numpy as np
 from tqdm import tqdm
@@ -118,37 +118,32 @@ def tile_body_water_ratio(image: np.ndarray) -> float:
     return u_cnt[1] / len(nan_filtered)
 
 
-def mask_body_ratio_from_threshold(gt_list: list, ratio_threshold: float, label: str) -> np.ndarray:
+def mask_body_ratio_from_threshold(labels: List[Path], ratio_threshold: float, label: str) -> np.ndarray:
     """
     Returns a binary mask with the images having a body water ratio above the threshold.
     """
-
     # for each mask in the path read the image array and get the tile body water ratio
-    assert len(gt_list) > 0, "No masks found in the path"
+    assert len(labels) > 0, "No masks found in the path"
+    target_file = Path("data") / f"mask_{label}_t{ratio_threshold:.2f}.npy"
 
-    # check if the mask has been cached already
-    if (glob(f'data/cache/_mask_{label}_t{str(ratio_threshold)[-2:]}.npy')):
-
-        mask = np.load(f'./data/cache/_mask_{label}_t{str(ratio_threshold)[-2:]}.npy')
+    # early out if the mask array has been cached already
+    if target_file.exists() and target_file.is_file():
+        mask = np.load(str(target_file))
         _, counts = np.unique(mask, return_counts=True)
-
         return mask, counts
 
     # create the list for filtering the elements, set to zero by default
-    mask = np.zeros(len(gt_list))
-
-    for i, gt in enumerate(tqdm(gt_list)):
+    mask = np.zeros(len(labels))
+    for i, label_path in enumerate(tqdm(labels)):
         # 1. read the image
         # 2. get the body water ratio
         # 3. if the ratio is above the threshold, set the mask to 1, meaning use the image
-        image = imread(gt)
+        image = imread(label_path)
         ratio = tile_body_water_ratio(image)
         if ratio >= ratio_threshold:
             mask[i] = 1
 
     _, counts = np.unique(mask, return_counts=True)
-
     # save a cache file of the mask for future usage
-    np.save(f'./data/cache/_mask_{label}_t{str(ratio_threshold)[-2:]}.npy', mask)
-
+    np.save(str(target_file), mask)
     return mask, counts